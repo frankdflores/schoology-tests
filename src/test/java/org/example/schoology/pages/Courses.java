7b40c594f6fec6fe2b829825047bfeecbf26c40b
jaru84 <rrosalesj@gmail.com>
2020-05-27 09:45:10 -0400

    jacky_rosales/edit_course

--- src/test/java/org/example/schoology/pages/Courses.java
+++ src/test/java/org/example/schoology/pages/Courses.java
@@ -1,13 +1,66 @@
 package org.example.schoology.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Courses {
 
 	private WebDriver driver;
+	private String name, section,area,level;
 
 	public Courses(WebDriver driver) {
 		this.driver = driver;
 	}
 
+	public Courses(String name, String section, String area, String level) {
+		this.name = name;
+		this.section = section;
+		this.area = area;
+		this.level = level;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+		driver.findElement(By.cssSelector("#edit-course-name")).sendKeys(name);
+	}
+
+	public void setSection (String section) {
+		this.name = section;
+		WebElement sectionField = driver.findElement(By.cssSelector("#edit-section-name-1"));
+		sectionField.clear();
+		sectionField.sendKeys(section);
+	}
+
+	public void setArea (String area) {
+		this.name = area;
+		Select subjectArea = new Select(driver.findElement(By.cssSelector("#edit-subject-area")));
+		subjectArea.selectByVisibleText(area);
+	}
+
+	public void setLevel (String nlevel) {
+		this.name = level;
+		Select level = new Select(driver.findElement(By.cssSelector("#edit-grade-level-range-start")));
+		level.selectByVisibleText(nlevel);
+	}
+
+	public void clickCreateCourseBtn() {
+		driver.findElement(By.cssSelector("a.create-course-btn")).click();
+	}
+
+	public void fillFormNewCourse(String name, String section, String area, String nlevel) {
+		setName(name);
+		setSection(section);
+		setArea(area);
+		setLevel(nlevel);
+	}
+	public void submitCourse(){
+		driver.findElement(By.cssSelector("#edit-submit")).click();
+	}
+
+	public void editCourse(String courseName) {
+		String courseActions = "//span[text()='%s']/ancestor::li//div[@class='action-links-unfold ']";
+		driver.findElement(By.xpath(String.format(courseActions, courseName))).click();
+	}
 }
--- src/test/java/org/example/schoology/tests/CoursesTest.java
+++ src/test/java/org/example/schoology/tests/CoursesTest.java
@@ -11,30 +11,18 @@ public class CoursesTest {
 	@Test
 	public void editCourse() {
 		Login login = new Login();
-		Home home = login.loginAs("carledriss+01@gmail.com", "P@ssw0rd");
+		Home home = login.loginAs("jacky.rosales84@gmail.com", "Control123");
 		SubMenu subMenu = home.clickMenu("Courses");
 		Courses courses = subMenu.clickMyCoursesLink();
 
-//		driver.findElement(By.cssSelector("a.create-course-btn")).click();
-//
-//		String courseName = "Test Course";
-//		driver.findElement(By.cssSelector("#edit-course-name")).sendKeys(courseName);
-//		WebElement sectionField = driver.findElement(By.cssSelector("#edit-section-name-1"));
-//		sectionField.clear();
-//		sectionField.sendKeys("Section");
-//		Select subjectArea = new Select(driver.findElement(By.cssSelector("#edit-subject-area")));
-//		subjectArea.selectByVisibleText("Mathematics");
-//		Select level = new Select(driver.findElement(By.cssSelector("#edit-grade-level-range-start")));
-//		level.selectByVisibleText("Undergraduate");
-//
-//		driver.findElement(By.cssSelector("#edit-submit")).click();
-//
-//		driver.findElement(By.xpath("//span[text()='Courses']/parent::button")).click();
-//
-//		driver.findElement(By.cssSelector("a[href='/courses']")).click();
-//
-//		String courseActions = "//span[text()='%s']/ancestor::li//div[@class='action-links-unfold ']";
-//		driver.findElement(By.xpath(String.format(courseActions, courseName))).click();
+		courses.clickCreateCourseBtn();
+		courses.fillFormNewCourse("Test Course", "section A", "Mathematics", "Undergraduate");
+		courses.submitCourse();
+
+		subMenu = home.clickMenu("Courses");
+		courses = subMenu.clickMyCoursesLink();
+		courses.editCourse("Test Course");
+
 	}
 
 }
