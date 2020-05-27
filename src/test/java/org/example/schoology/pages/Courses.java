c370856e200efe18b58f1c2a197437e2b2a080eb
FranzVidangosPari <Franz.Vidangos@jalasoft.com>
2020-05-27 11:40:21 -0400

    Franz-Vidangos/task04: add createModal

--- src/test/java/org/example/schoology/pages/Courses.java
+++ src/test/java/org/example/schoology/pages/Courses.java
@@ -1,13 +1,25 @@
 package org.example.schoology.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Courses {
 
 	private WebDriver driver;
 
+	@FindBy(css = "a.create-course-btn")
+	WebElement createCourse;
+
 	public Courses(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public CreateModal openModal(){
+		createCourse.click();
+		return new CreateModal(driver);
 	}
 
 }
