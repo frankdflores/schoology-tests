82f20b8955a2e3e57cbe48136ca93bd4d99ef7f6
Mauricio <zelaya.mauricio@gmail.com>
2020-05-27 02:07:39 +0000

    refactoring code for homework

--- src/test/java/org/example/schoology/pages/Courses.java
+++ src/test/java/org/example/schoology/pages/Courses.java
@@ -1,13 +1,30 @@
 package org.example.schoology.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Courses {
 
 	private WebDriver driver;
 
+	@FindBy(css = "a.create-course-btn")
+	WebElement createCourseButton;
+
 	public Courses(WebDriver driver) {
+
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public CreateCourseModal openCourseModal(){
+
+		createCourseButton.click();
+		return new CreateCourseModal(driver);
+
 	}
 
 }
---
+++ src/test/java/org/example/schoology/pages/CreateCourseModal.java
@@ -0,0 +1,45 @@
+package org.example.schoology.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateCourseModal {
+
+    WebDriver driver;
+
+    @FindBy(css = "#edit-course-name")
+    WebElement editCourseName;
+
+    @FindBy(css = "#edit-section-name-1")
+    WebElement sectionField;
+
+    @FindBy(css = "#edit-subject-area")
+    WebElement editSubjectArea;
+
+    @FindBy(css = "#edit-grade-level-range-start")
+    WebElement editGradeLevelRangeStart;
+
+    @FindBy(css = "a[href='/courses']")
+    WebElement createCourseButton;
+
+    public CreateCourseModal(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void createNewCourse(String courseName){
+
+        editCourseName.sendKeys(courseName);
+		sectionField.clear();
+		sectionField.sendKeys("Section");
+		Select subjectArea = new Select(editSubjectArea);
+		subjectArea.selectByVisibleText("Mathematics");
+		Select level = new Select(editGradeLevelRangeStart);
+		level.selectByVisibleText("Undergraduate");
+        createCourseButton.click();
+
+    }
+}
