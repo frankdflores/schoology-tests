e7a1e47b33af6c4df257ede0de5c2bc7cbc89784
Benjamin Huanca Aliaga <Benjamin Huanca@jalasoft.local>
2020-07-14 18:46:42 -0400

    Refactoring groups and courses

--- src/test/java/org/example/schoology/pages/courses/CreateCoursePopup.java
+++ src/test/java/org/example/schoology/pages/courses/CreateCoursePopup.java
@@ -1,13 +1,70 @@
 package org.example.schoology.pages.courses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class CreateCoursePopup extends AbstractCoursePopup {
+    WebDriver driver;
 
-    public Course create(final Map<String, String> courseMap) {
-        fill(courseMap);
-        submitButton.click();
-        return new Course();
+    @FindBy(css = "#edit-course-name")
+    WebElement courseNameTextField;
+
+    @FindBy(css = "#edit-section-name-1")
+    WebElement sectionTextField;
+
+    @FindBy(css = "#edit-subject-area")
+    WebElement editSubjectArea;
+
+    @FindBy(css = "#edit-grade-level-range-start")
+    WebElement editGradeLevelRangeStart;
+
+    @FindBy(css = "a[href='/courses']")
+    WebElement createCourseButton;
+
+    public CreateCoursePopup(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public void create(Map<String, String> courseName){
+
+        Map<String, Step> stepsMap = new HashMap<>();
+
+        stepsMap.put("name", () -> setName(courseName.get("name")));
+        stepsMap.put("section", () -> setSection(courseName.get("section")));
+        stepsMap.put("area", () -> selectSubjectArea(courseName.get("area")));
+        stepsMap.put("level", () -> selectLevel(courseName.get("level")));
+
+        for(String keyField : courseName.keySet()){
+            stepsMap.get(keyField).execute();
+        }
+
+        createCourseButton.click();
+
     }
 
+    public void setName(String name){
+        courseNameTextField.sendKeys(name);
+    }
+
+    public void setSection(String section){
+        WebElement sectionField = sectionTextField;
+        sectionTextField.clear();
+        sectionTextField.sendKeys(section);
+    }
+
+    public void selectSubjectArea(String area){
+        Select subjectArea = new Select(editSubjectArea);
+        subjectArea.selectByVisibleText(area);
+    }
+
+    public void selectLevel (String level){
+        Select levelDropDown = new Select(editGradeLevelRangeStart);
+        levelDropDown.selectByVisibleText(level);
+    }
 }
--- src/test/java/org/example/schoology/pages/groups/Groups.java
+++ src/test/java/org/example/schoology/pages/groups/Groups.java
@@ -1,40 +1,27 @@
 package org.example.schoology.pages.groups;
-
-import org.example.schoology.pages.ViewList;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.PageFactory;
 
-public class Groups extends ViewList {
+public class Groups {
 
-    public static final String GROUP_ACTIONS_BUTTON = "//a[text()='%s']/ancestor::li//div[@href='#']";
-    public static final String SELECT_ACTIONS = "//a[text()='%s']/ancestor::li//ul//li[@class='action-edit']";
-    public static final String GROUP_BY_NAME = "//a[text()='%s']";
+    private WebDriver driver;
 
     @FindBy(css = "a.create-group")
-    private WebElement createGroupButton;
+    WebElement createGroup;
 
-    public CreateGroupPopup clickCreateGroupButton() {
-        createGroupButton.click();
-        return new CreateGroupPopup();
+    public Groups(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
     }
 
-    public EditGroupPopup clickEditGroup(final String groupName) {
-        WebElement groupActionsButton = driver.findElement(By.xpath(String.format(GROUP_ACTIONS_BUTTON, groupName)));
-        // Scroll
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", groupActionsButton);
+    public CreateGroupPopup openGroupsModal(){
 
-        wait.until(ExpectedConditions.visibilityOf(groupActionsButton));
-        groupActionsButton.click();
-        driver.findElement(By.xpath(String.format(SELECT_ACTIONS, groupName))).click();
-        return new EditGroupPopup();
+        createGroup.click();
+        return new CreateGroupPopup(driver);
+
     }
-
-    public String getGroupByName(final String groupName) {
-        return driver.findElement(By.xpath(String.format(GROUP_BY_NAME, groupName))).getText();
-    }
-
 }
